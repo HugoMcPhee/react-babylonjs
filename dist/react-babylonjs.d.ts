f6cc832da4c847e9d8fd8c1f43114a69a6cb13e2
Hugo McPhee <mcphee.hugo@gmail.com>
2020-05-31 12:27:37 +1000

    updated the dist to the newest

---
+++ dist/react-babylonjs.d.ts
@@ -0,0 +1,9 @@
+export * from "./generatedCode";
+export * from "./generatedProps";
+export * from "./hooks";
+export * from "./customComponents";
+export * from "./PropsHandler";
+export { applyInitialPropsToInstance } from './UpdateInstance';
+export { default as Engine, withBabylonJS, BabylonJSContext, useBabylonEngine, useBabylonCanvas } from "./Engine";
+export { default as Scene, withScene, WithSceneContext, SceneContext, SceneEventArgs, useBabylonScene } from "./Scene";
+export { HostWithEvents } from "./customHosts";
